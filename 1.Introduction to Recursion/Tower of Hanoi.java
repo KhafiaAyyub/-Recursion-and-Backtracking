38042e4ea5495c3402f6167fc3a1368cdaa2a80f
KhafiaAyyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-07-22 20:55:07 +0530

    Update Tower of Hanoi.java

--- 1.Introduction to Recursion/Tower of Hanoi.java
+++ 1.Introduction to Recursion/Tower of Hanoi.java
@@ -43,31 +43,28 @@ Sample Output
 1[10 -> 11]
 
 */
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Scanner sc =  new Scanner(System.in);
-        int n = sc.nextInt();
-        pzz(n);
+    Scanner sc =  new Scanner(System.in);
+    int n = sc.nextInt();
+    int t1 = sc.nextInt();
+    int t2  = sc.nextInt();
+    int t3 = sc.nextInt();
+    toh(n,t1,t2,t3);
     }
 
-    public static void pzz(int n){
-     if(n==0){
-         return;
-     }
-     
-     System.out.print(n + " ");
-     pzz(n-1);
-     System.out.print(n + " ");
-     pzz(n-1);
-     System.out.print(n+ " ");
-     
+    public static void toh(int n, int t1, int t2, int t3){
+        if(n==0){
+            return;
+        }
         
+        toh(n-1,t1,t3,t2);  //will print the instruction to move n-1 from t1 to t3 using t2 (*)
+        System.out.println(n + "[" + t1 + " -> " + t2 + "]");
+        toh(n-1,t3,t2,t1);
     }
 
 }
-

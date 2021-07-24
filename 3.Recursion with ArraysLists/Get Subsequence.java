a79b0f18a4e9970ecd8b57e56209c8abc2ab252c
KhafiaAyyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-07-24 21:45:18 +0530

    Create Get Subsequence.java

---
+++ 3.Recursion with ArraysLists/Get Subsequence.java
@@ -0,0 +1,22 @@
+/* Get Subsequence
+Easy 
+
+1. You are given a string str.
+2. Complete the body of getSS function - without changing signature - to calculate all subsequences of str.
+
+Input Format
+A string str
+
+Output Format
+Contents of the arraylist containing subsequences as shown in sample output
+
+Constraints
+0 <= str.length <= 20
+
+Sample Input
+abc
+
+Sample Output
+[, c, b, bc, a, ac, ab, abc]
+
+*/

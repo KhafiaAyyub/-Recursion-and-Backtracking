0c7cca7d193df935e5bb3e4c36e5c22c0779c29a
KhafiaAyyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-07-25 22:44:39 +0530

    Update Get Subsequence.java

--- 3.Recursion with ArraysLists/Get Subsequence.java
+++ 3.Recursion with ArraysLists/Get Subsequence.java
@@ -20,3 +20,40 @@ Sample Output
 [, c, b, bc, a, ac, ab, abc]
 
 */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        ArrayList<String> res = gss(str);
+        System.out.println(res);
+    }
+
+    public static ArrayList<String> gss(String str) {
+        
+        if(str.length() == 0){
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return  bres;
+        }
+        char ch = str.charAt(0); //a
+        String ros = str.substring(1);//bc
+        ArrayList<String> rres = gss(ros);//[--,-c,b--,bc]
+
+        ArrayList<String> mres = new ArrayList<>();
+        for(String rstr:rres){
+            mres.add(""+ rstr);
+           
+        }
+        for(String rstr:rres){
+          
+            mres.add(ch + rstr);
+        }
+        return mres;
+    }
+
+}

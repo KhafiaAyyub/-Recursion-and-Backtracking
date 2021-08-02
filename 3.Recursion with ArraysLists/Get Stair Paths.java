3b31045a2ffb65c2820b5bc5df2aa7521cd27ef8
KhafiaAyyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-08-02 12:39:49 +0530

    Update Get Stair Paths.java

--- 3.Recursion with ArraysLists/Get Stair Paths.java
+++ 3.Recursion with ArraysLists/Get Stair Paths.java
@@ -3,7 +3,7 @@ Easy
 
 1. You are given a number n representing number of stairs in a staircase.
 2. You are standing at the bottom of staircase. You are allowed to climb 1 step, 2 steps or 3 steps in one move.
-3. Complete the body of getStairPaths function - without changing signature - to get the list of all paths that can be used to climb the staircase up.
+3. Complete the body of getStairPaths function - to get the list of all paths that can be used to climb the staircase up.
 
 Input Format
 A number n

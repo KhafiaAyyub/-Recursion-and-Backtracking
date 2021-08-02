05ca485426d3929232c6b6ab9087616b28ddb398
KhafiaAyyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-08-02 12:38:37 +0530

    Update Get KPC.java

--- 3.Recursion with ArraysLists/Get KPC.java
+++ 3.Recursion with ArraysLists/Get KPC.java
@@ -1,5 +1,6 @@
 /* Get Kpc
 Easy  
+
 1. You are given a string str. The string str will contains numbers only, where each number stands for a key pressed on a mobile phone.
 2. The following list is the key to characters map :
     0 -> .;
@@ -12,18 +13,23 @@ Easy
     7 -> tu
     8 -> vwx
     9 -> yz
-3. Complete the body of getKPC function - without changing signature - to get the list of all words that could be produced by the keys in str.
+    
 Input Format
 A string str
+
 Output Format
 Contents of the arraylist containing words as shown in sample output
+
 Constraints
 0 <= str.length <= 10
 str contains numbers only
+
 Sample Input
 78
+
 Sample Output
 [tv, tw, tx, uv, uw, ux]
+
 */
 import java.io.*;
 import java.util.*;

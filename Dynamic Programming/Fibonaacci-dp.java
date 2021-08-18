5e34666661bae4d418ac116e6e06ec4b9352304d
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-08-18 22:37:30 +0530

    Update Fibonaacci-dp.java

--- Dynamic Programming/Fibonaacci-dp.java
+++ Dynamic Programming/Fibonaacci-dp.java
@@ -30,3 +30,35 @@ Sample Output
 
 */
 
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] dp = new int[n+1];
+    
+    System.out.println(fibonacci(n, dp));
+ }
+ 
+ public static int fibonacci(int n, int[] dp){
+    if(n==0 || n==1){
+        return n;
+    }
+    
+    if(dp[n]!=0){
+        return dp[n];
+    }
+    
+    int a = fibonacci(n-1, dp);
+    int b = fibonacci(n-2, dp);
+    int c = a+b;
+    dp[n] = c;
+    return c;
+ }
+}
+
